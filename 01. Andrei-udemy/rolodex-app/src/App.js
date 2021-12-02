5c276e16133685dc41d3f4bea008f9b109d232a1
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-12-02 22:02:48 +0100

    filter

--- 01. Andrei-udemy/rolodex-app/src/App.js
+++ 01. Andrei-udemy/rolodex-app/src/App.js
@@ -1,30 +1,37 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: "",
     };
   }
 
-  componentDidMount() { //onInit
+  componentDidMount() {
+    //onInit
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    
     return (
       <div className="App">
-        <CardList name="Atesz">
-          <h1>Atesz</h1>
-        </CardList>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}> {monster.name}</h1>
-        ))}
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value} )}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
