9b06410d1a97ef0af22577621ada7242defc1dca
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-12-05 21:41:16 +0100

    crwn project init

---
+++ 01. Andrei-udemy/crwn-clothing/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import HomePage from "./components/home-page/home-page.component";
+
+import "./App.css";
+
+function App() {
+  return <div><HomePage /></div>;
+}
+
+export default App;
