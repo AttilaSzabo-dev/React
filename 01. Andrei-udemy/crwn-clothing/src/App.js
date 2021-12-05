710ce0e8a4d3276ab245bfe30f5086c1375eb0fc
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-12-05 23:01:46 +0100

    home page design

--- 01. Andrei-udemy/crwn-clothing/src/App.js
+++ 01. Andrei-udemy/crwn-clothing/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import HomePage from "./components/home-page/home-page.component";
+import HomePage from "./pages/home-page/home-page.component";
 
 import "./App.css";
 
