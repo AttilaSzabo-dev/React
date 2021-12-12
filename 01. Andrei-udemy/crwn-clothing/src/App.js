226d7b421d0c886eb376be8a9f0285ee004f1d58
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-12-12 22:46:53 +0100

    router

--- 01. Andrei-udemy/crwn-clothing/src/App.js
+++ 01. Andrei-udemy/crwn-clothing/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import HomePage from "./pages/home-page/home-page.component";
 
 import "./App.css";
 
 function App() {
-  return <div><HomePage /></div>;
+  return (
+    <div>
+      <Route path="/">
+        <HomePage/>
+      </Route>
+    </div>
+  );
 }
 
 export default App;
