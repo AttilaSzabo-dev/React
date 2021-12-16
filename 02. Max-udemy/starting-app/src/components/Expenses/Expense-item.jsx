93efd37204f46f276e6d29819188b1c5e2285732
Attila Szabo <attila.szabo.vrc@gmail.com>
2021-12-16 19:06:55 +0100

    section 3 finished

---
+++ 02. Max-udemy/starting-app/src/components/Expenses/Expense-item.jsx
@@ -0,0 +1,18 @@
+import ExpenseDate from "./Expense-date";
+import Card from "../UI/Card";
+
+import "./Expense-item.scss";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
