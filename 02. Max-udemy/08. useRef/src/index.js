d55e585d770453d36b62573ca0db80fe9d1a4fac
Attila Szabo <attila.szabo.vrc@gmail.com>
2022-02-06 21:56:24 +0100

    init

---
+++ 02. Max-udemy/08. useRef/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { AuthContextProvider } from "./context/auth-context";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
+  document.getElementById("root")
+);
