110e81a609c82a3574370c31a6ae6ab033b81648
Attila Szabo <attila.szabo.vrc@gmail.com>
2022-02-17 19:47:54 +0100

    media querryik

--- 03. Projects/blog_layer/js/blog.js
+++ 03. Projects/blog_layer/js/blog.js
@@ -1,5 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("show-more").addEventListener("click", showMorePosts);
+
+  var slider = tns({
+    container: ".inda-slider",
+    items: 1,
+    slideBy: 1,
+    responsive: {
+      0: {
+        //gutter: 55,
+        //edgePadding: 10
+      },
+      860: {
+        items: 2,
+        gutter: 55,
+      },
+      1120: {
+        items: 3,
+        gutter: 55,
+      },
+    },
+    gutter: 55,
+    edgePadding: 10,
+    //autoWidth: true,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3500,
+    mouseDrag: true,
+    swipeAngle: false,
+    speed: 400,
+    prevButton: "#slider_prev",
+    nextButton: "#slider_next",
+    nav: true,
+    navPosition: "bottom",
+  });
 });
 
 const showMorePosts = () => {
