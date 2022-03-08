e58be354054a55ea5bf0b1ad05a48d71bce0ea3f
Attila Szabo <attila.szabo.vrc@gmail.com>
2022-03-08 16:46:32 +0100

    fetch test

--- 03. Projects/port_tv/src/App.js
+++ 03. Projects/port_tv/src/App.js
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [test, setTest] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const resp = await fetch("https://szaboa-3.dev.port.hu/tv-event/init"); 
+      setTest(await resp.json());
+      
+      console.log(test);
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       
