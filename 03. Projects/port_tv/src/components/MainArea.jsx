9071e869143189102a7d22d6e2e4a0606f8e59df
Attila Szabo <attila.szabo.vrc@gmail.com>
2022-03-08 15:27:19 +0100

    main components

---
+++ 03. Projects/port_tv/src/components/MainArea.jsx
@@ -0,0 +1,11 @@
+import classes from "./MainArea.module.css";
+
+const MainArea = props => {
+    return (
+        <div className="mainArea">
+            
+        </div>
+    );
+};
+
+export default MainArea;
