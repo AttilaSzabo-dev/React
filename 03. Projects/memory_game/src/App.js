10b5faacd81b363bc6c76a6b8ea01ea14a3f8e9e
Attila Szabo <attila.szabo.vrc@gmail.com>
2023-02-03 02:21:08 +0100

    basic algorithm

--- 03. Projects/memory_game/src/App.js
+++ 03. Projects/memory_game/src/App.js
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [data, setData] = useState(null);
+  const [playField, setPlayField] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  useEffect(() => {
+    if (data !== null) {
+      createBlocks();
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (playField !== null) {
+      //console.log(playField);
+    }
+  }, [playField]);
+
+  const createBlocks = () => {
+    const baseData = data.map((card) => card).filter((card) => card.id <= 8);
+    let finalData = [];
+
+    for (let i = 0; i < baseData.length; i++) {
+      for (let y = 0; y < 2; y++) {
+        finalData.splice(Math.floor(Math.random() * (8 * 2)), 0, baseData[i].id)
+      }
+    }
+    /* finalData.forEach((item) => {
+      setPlayField(prev => [
+        ...prev,
+        baseData.map(card => card).some(card => item === card.id)
+      ])
+    }) */
+    console.log(finalData);
+  };
+
+
+  return (
+    <div>
+
+    </div>
+  );
 }
 
 export default App;
