30fe193bcd29ac6eda1bde2d7164ec0b457fb714
Attila Szabo <attila.szabo.vrc@gmail.com>
2024-06-04 21:34:39 +0200

    tech change

--- 03. Projects/picture_store/src/App.tsx
+++ 03. Projects/picture_store/src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "pages/HomePage";
+
 function App(): JSX.Element {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
